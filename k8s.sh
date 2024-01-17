cb5d4c4773df255ede60ff93bc3456c9ce830dd1
KKISO574 <wang2911074976@outlook.com>
2024-01-17 16:19:21 +0800

    up

--- k8s.sh
+++ k8s.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+## 权限判定
+function PermissionJudgment() {
+    if [ $UID -ne 0 ]; then
+        Output_Error "权限不足，请使用 Root 用户运行本脚本"
+        exit
+    fi
+}
+
+
+echo "开始安装"
+
 yum install -y ipset ipvsadm vim wget curl net-tools
 function set_base(){
 # 关闭防火墙，PS：如果使用云服务器，还需要在云服务器的控制台中把防火墙关闭了或者允许所有端口。
@@ -28,8 +39,43 @@ EOF
 sysctl -p /etc/sysctl.d/k8s.conf
 # iptables生效参数
   sysctl --system
-}
 
+sleep 1
+echo "开始导入Docker镜像源"
+#docker阿里源
+yum install -y yum-utils 
+yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
+
+
+echo "导入k8s阿里源"
+sleep 1
+#k8s阿里云源
+cat > /etc/yum.repos.d/kubernetes.repo << EOF
+[kubernetes]
+name=Kubernetes
+baseurl=https://mirrors.aliyun.com/kubernetes/yum/repos/kubernetes-el7-x86_64
+enabled=1
+gpgcheck=0
+repo_gpgcheck=0
+gpgkey=https://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg https://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
+EOF
+
+echo "变更镜像加速源以及cgroup"
+sleep 1
+
+cat <<EOF | sudo tee /etc/docker/daemon.json
+{
+	"registry-mirrors": [
+        "http://hub-mirror.c.163.com",
+        "https://docker.mirrors.ustc.edu.cn",
+        "https://registry.docker-cn.com"
+    ],
+	"exec-opts": ["native.cgroupdriver=systemd"]
+}
+EOF
+  
+}
+PermissionJudgment
 set_base
 lsmod | grep br_netfilter
 echo "5秒后重启"
